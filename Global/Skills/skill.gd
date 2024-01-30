f13614702e0d6ef2c1d74b9fb65f0d33a6b48367
Juanxpeke <juanintendo.f11@gmail.com>
2024-01-29 22:06:02 -0300

    Little changes

--- Global/Skills/skill.gd
+++ Global/Skills/skill.gd
@@ -5,3 +5,9 @@ extends Resource
 @export_multiline var description : String
 @export var manas_needed : Array[int] = [0, 0, 0, 0]
 @export var sprite : Texture2D
+
+# Public
+
+# Applies the skill effect to the given entity
+func apply_effect(entity : Entity) -> void:
+	pass
---
+++ Global/Skills/skill_manager.gd
@@ -0,0 +1,13 @@
+extends Node
+
+var skills = [
+	load("res://Global/Skills/Fireball/fireball.tres"),
+]
+
+# Private
+
+# Called when the node enters the scene tree for the first time
+func _ready() -> void:
+	pass # Replace with function body
+
+
