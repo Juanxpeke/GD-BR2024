1a296aaf8164ff0410327924e3e9318546ac1c7d
Giarettini <antogiaesc@gmail.com>
2024-02-04 13:27:14 -0300

    Added extra art and polished some tutorial images

--- Core/GUI/Tutorial/tutorial.gd
+++ Core/GUI/Tutorial/tutorial.gd
@@ -8,11 +8,13 @@ var current_panel : int = 0
 @onready var label := %Label
 @onready var previous_button = %Previous
 @onready var next_button = %Next
+@onready var exit_button = %ExitButton
 
 #Private
 func _ready() -> void:
 	previous_button.pressed.connect(set_previous_panel)
 	next_button.pressed.connect(set_next_panel)
+	exit_button.pressed.connect(close_tutorial)
 	print(tutorial_panels.size())
 	_init_node()
 
@@ -22,16 +24,22 @@ func _init_node() -> void:
 
 # Sets the next tutorial panel
 func set_next_panel() -> void:
-	current_panel +=1
-	if current_panel >= tutorial_panels.size():
-		self.queue_free()
+	if current_panel == tutorial_panels.size() - 1:
+		return
 	else:
+		current_panel += 1
 		texture.texture = tutorial_panels[current_panel].panel_image
 		label.text = tutorial_panels[current_panel].panel_text
 
 
 # Sets the previous tutorial panel
 func set_previous_panel() -> void:
-	current_panel -= 1
-	texture.texture =tutorial_panels[current_panel].panel_image
-	label.text = tutorial_panels[current_panel].panel_text
+	if current_panel == 0:
+		pass
+	else:
+		current_panel -= 1
+		texture.texture =tutorial_panels[current_panel].panel_image
+		label.text = tutorial_panels[current_panel].panel_text
+
+func close_tutorial() -> void:
+	queue_free()
