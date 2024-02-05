aa54a2582534f93b5023fe9e45ee144f071bec94
Juanxpeke <juanintendo.f11@gmail.com>
2024-02-05 13:28:03 -0300

    Improved UI

--- Core/GUI/Credits/Credits.gd
+++ Core/GUI/Credits/Credits.gd
@@ -6,6 +6,7 @@ extends Control
 
 # Called when the node enters the scene tree for the first time
 func _ready() -> void:
+	exit_button.button_down.connect(func(): AudioManager.play_sound(AudioManager.click_sound))
 	exit_button.pressed.connect(_on_exit_button_pressed)
 
 #Called when exit button is pressed
